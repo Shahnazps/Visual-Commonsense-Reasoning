d7bf7d594f7810f82f9b564ed9fbd8c7fd153b84
Shahnazps <shahnaz.ps@gmail.com>
2021-06-27 13:20:43 +0530

    path issue resolved
    Signed-off-by: Shahnazps <shahnaz.ps@gmail.com>

--- project.py
+++ project.py
@@ -358,4 +358,4 @@ def modelOutput(input:ImageNo)->OutputImage:
     # q = listToString(question)
     # print("q converted : ",q)
     #return OutputImage(image=loadImage(imagePath,input.index),question=q,answer1=listToString(ans1),answer2=listToString(ans2),answer3=listToString(ans3),answer4=listToString(ans4))
-    return OutputImage(image=loadImage(imagePath,modelIndex),answer = input.userChoice)
+    return OutputImage(image=loadImage(imagePath1,modelIndex),answer = input.userChoice)
